094cef6cfb4ce1372e33bfcd7bff30d1bc513da3
nareshbille <nareshbille12@gmail.com>
2025-10-27 23:48:26 +0530

    refactored prod properties and updated the SecurityConfig code

--- src/main/java/com/example/care_management_system/security/SecurityConfig.java
+++ src/main/java/com/example/care_management_system/security/SecurityConfig.java
@@ -57,7 +57,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://127.0.0.1:5500", "http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of("http://127.0.0.1:5500", "http://127.0.0.1:5501", "http://localhost:3000"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true); // if your frontend sends cookies or auth headers
